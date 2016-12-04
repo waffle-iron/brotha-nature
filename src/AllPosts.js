2995515555492584adb60c1159e567deddf15ee1
Caleb <calebcowen@gmail.com>
2016-12-04 08:43:48 -0700

    Style page a little

--- src/AllPosts.js
+++ src/AllPosts.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
+import Post from './Post'
 
 class AllPosts extends Component {
   render(){
+    const postStyle = {
+      textAlign: "center"
+    }
     var posts = this.props.posts.map((post) => {
       return(
        <div key={post.id}>
-         <img src={post.image.url} role="presentation" style={{width: 150, height: 150}} />
-        <h3>{ post.title }</h3>
-        <p>{ post.body }</p>
+         <Post post={post} />
        </div>
       )
     });
     return(
-      <div>
-        <h1>Posts</h1>
+      <div style={ postStyle }>
+        <h1>Brotha Nature</h1>
         { posts }
       </div>
     )
---
+++ src/Post.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+  render() {
+    const bodyStyle = {
+      marginLeft: 240,
+      marginRight: 240,
+      marginBottom: 50
+    }
+    return (
+      <div>
+        <img src={this.props.post.image.url}
+        role="presentation"
+        style={{width: 300, height: 400}} />
+
+        <h3>{ this.props.post.title }</h3>
+        <p style={bodyStyle}>{ this.props.post.body }</p>
+      </div>
+    )
+  }
+}
+
+export default Post
