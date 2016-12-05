18181a3bfe0998a6c7a759943734bef557688f32
Caleb <calebcowen@gmail.com>
2016-12-05 13:38:23 -0700

    Add more margin to post bodt

--- src/Post.js
+++ src/Post.js
@@ -18,8 +18,8 @@ class Post extends Component {
     }
 
     let pStyle = {
-      marginLeft: 10,
-      marginRight: 10,
+      marginLeft: 15,
+      marginRight: 15,
       fontFamily: "sans-sarif",
       fontSize: 20,
       fontWeight: "bold",
